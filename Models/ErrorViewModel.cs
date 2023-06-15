de03311950e32488b624cd58548f771f24372f4a
bseton11-YJKnqQ <bseton11@gmail.com>
2023-06-15 13:30:52 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace nextjsarduinosolution.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
